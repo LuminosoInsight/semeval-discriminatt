8061d70ff2cdf02cddfde173f7504e5b38879ab5
Rob Speer <rob@luminoso.com>
2018-01-08 16:00:53 -0500

    Python skeleton

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="semeval-discriminatt",
+    version='0.1',
+    maintainer='Luminoso Technologies, Inc.',
+    maintainer_email='rspeer@luminoso.com',
+    platforms=["any"],
+    description="Attempting SemEval-2018 task 10: Capturing Discriminative Attributes",
+    packages=['discriminatt'],
+    install_requires=['wordfreq', 'numpy', 'pandas', 'scikit-learn'],
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
