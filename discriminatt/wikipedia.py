c8563d5558dc1711bddbdb6f47b63b972af3fa3e
Rob Speer <rob@luminoso.com>
2018-01-18 13:20:06 -0500

    include wikipedia.py

---
+++ discriminatt/wikipedia.py
@@ -0,0 +1,13 @@
+import sqlite3
+from conceptnet5.language.english import LEMMATIZER
+from conceptnet5.nodes import standardized_concept_uri
+
+
+def wikipedia_connected_conceptnet_nodes(db, start):
+    nodes = set()
+    c = db.cursor()
+    c.execute("SELECT lemma FROM words WHERE page=? LIMIT 1000", (start.casefold(),))
+    for row in c.fetchall():
+        lemma = row[0]
+        nodes.add(standardized_concept_uri('en', lemma))
+    return list(nodes)
