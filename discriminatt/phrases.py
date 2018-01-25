ab08817208e35422d7c5d896ea6e47f3bb81dd1c
jlowryduda <joanna.teresa.duda@gmail.com>
2018-01-25 14:40:53 -0500

    lemmas in phrases.py

--- discriminatt/phrases.py
+++ discriminatt/phrases.py
@@ -1,7 +1,7 @@
 
-def count_for_phrases(db, word, attribute):
+def phrase_count(db, lemma_word, lemma_attribute):
     c = db.cursor()
-    c.execute("SELECT count FROM phrases WHERE (first_word=? AND second_word=?) OR (first_word=? "
-              "AND second_word=?)", (word, attribute, attribute, word))
+    c.execute("SELECT count FROM phrases WHERE (first_lemma=? AND second_lemma=?) OR (first_lemma=? "
+              "AND second_lemma=?)", (lemma_word, lemma_attribute, lemma_attribute, lemma_word))
     results = c.fetchall()
     return sum([result[0] for result in results])
